6b4ea645d0de19467676791788020dfa5498eb3b
nitika0103 <104589664+nitika0103@users.noreply.github.com>
2022-05-13 13:08:53 +0530

    Update 5_Peer_Graded_Assignment_Questions.py

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -94,7 +94,7 @@ app.layout = html.Div(children=[
                            {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
                            ],
                   placeholder='Select a report type',
-                  style={'width':80%,'padding':'3px','font size':'20px','textalign':'centre'})
+                  style={'width':'80%','padding':'3px','font-size':'20px','textalign':'centre'})
                                     # Place them next to each other using the division style
                                     ], style={'display':'flex'}),
                                     
@@ -137,11 +137,11 @@ app.layout = html.Div(children=[
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-@app.callback( [[Output(component_id='plot1', component_property='children'),
- Output("plot2",'children'),
- Output("plot3",'children'),
- Output("plot4",'children'),
- Output("plot5",'children')]],
+@app.callback([Output(component_id='plot1', component_property='children'),
+ Output(component_id='plot2', component_property='children'),
+ Output(component_id='plot3',component_property='children'),
+ Output(component_id='plot4',component_property='children'),
+ Output(component_id='plot5',component_property='children')],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
@@ -164,7 +164,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            line_fig = px.line(line_data, x='month', y='airline', color='Reporting_Airline', title='Average monthly flight time(minutes) by airline')
+            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average monthly flight time(minutes) by airline')
             
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
@@ -183,9 +183,9 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK6: Number of flights flying to each state from each reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            tree_fig = px.treemap(tree_data, path=['Deststate', 'Reporting_Airline'], 
-                      values='flights',
-                      color='flights',
+            tree_fig = px.treemap(tree_data, path=['DestState', 'Reporting_Airline'], 
+                      values='Flights',
+                      color='Flights',
                       color_continuous_scale='RdBu',
                       title='Flight count by airline to destination state'
                 )
